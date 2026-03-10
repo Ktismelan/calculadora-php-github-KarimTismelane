73c6e74719d28e4841f3e536412c5741587aae37
Karim <ktismelan@inscamidemar.cat>
2026-03-11 00:22:17 +0100

    Crear la funcion calcular

--- calculadora.php
+++ calculadora.php
@@ -1,21 +1,31 @@
 <?php
+
+function calcular($operacion, $a, $b) {
+
+    if ($operacion == "s") {
+        return $a + $b;
+    } else if ($operacion == "r") {
+        return $a - $b;
+    } else if ($operacion == "m") {
+        return $a * $b;
+    } else if ($operacion == "d") {
+
+        if ($b != 0) {
+            return $a / $b;
+        } else {
+            return "Error";
+        }
+
+    }
+
+}
+
 $operacion = $_POST['op'];
 $primerNumero = $_POST['n1'];
 $segundoNumero = $_POST['n2'];
 
-if ($operacion == "s") {
-    $resultado = $primerNumero + $segundoNumero;
-} else if ($operacion == "r") {
-    $resultado = $primerNumero - $segundoNumero;
-} else if ($operacion == "m") {
-    $resultado = $primerNumero * $segundoNumero;
-} else if ($operacion == "d") {
-    if ($segundoNumero != 0) {
-        $resultado = $primerNumero / $segundoNumero;
-    } else {
-        $resultado = "Error";
-    }
-}
+$resultado = calcular($operacion, $primerNumero, $segundoNumero);
 
 echo $resultado;
+
 ?>
